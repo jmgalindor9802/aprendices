e780f697ae774d0d92fa15f1e72074749aef3b15
jmgalindor9802 <jmgalindor9802@gmail.com>
2023-12-08 19:34:47 -0500

    tabla

--- js/ver.js
+++ js/ver.js
@@ -5,23 +5,11 @@ function ver() {
     db.collection("aprendices").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             const newRow = tableBody.insertRow();
-
             // Agregar celdas a la fila
-            newRow.insertCell().textContent = doc.data().Apellidos;
             newRow.insertCell().textContent = doc.data().Nombres;
-            newRow.insertCell().textContent = doc.data().acudiente;
-            newRow.insertCell().textContent = doc.data().barrio;
-            newRow.insertCell().textContent = doc.data().colegio;
-            newRow.insertCell().textContent = doc.data().correo;
-            newRow.insertCell().textContent = doc.data().fechaNacimiento;
-            newRow.insertCell().textContent = doc.data().jornada;
-            newRow.insertCell().textContent = doc.data().localidad;
-            newRow.insertCell().textContent = doc.data().municipio;
-            newRow.insertCell().textContent = doc.data().numDocumento;
-            newRow.insertCell().textContent = doc.data().programa;
-            newRow.insertCell().textContent = doc.data().telefono;
-            newRow.insertCell().textContent = doc.data().tipoAcudiente;
+            newRow.insertCell().textContent = doc.data().Apellidos;
             newRow.insertCell().textContent = doc.data().tipoDocumento;
+            newRow.insertCell().textContent = doc.data().numDocumento;
         });
     });
 }
